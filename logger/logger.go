d95a8ac52c1967c383a0552ef830e9f1bdf4f303
wind959 <15866422+wind959@users.noreply.github.com>
2025-05-22 18:12:30 +0800

    更新日志库fix

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,7 @@ package logutil
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"time"
 )
 
 // LogLevel 定义日志级别
@@ -53,13 +54,30 @@ func NewLogger(env LogLevel) *Logger {
 		config = zap.NewDevelopmentConfig()
 		config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
 	}
+	// 自定义 EncoderConfig
+	config.EncoderConfig = zapcore.EncoderConfig{
+		TimeKey:       "time",                           // 日志中时间字段的键名
+		LevelKey:      "level",                          // 日志中级别字段的键名
+		NameKey:       "logger",                         // 日志中记录器名称字段的键名
+		CallerKey:     "caller",                         // 日志中调用者信息字段的键名
+		FunctionKey:   zapcore.OmitKey,                  // 日志中函数名字段的键名，这里设置为忽略
+		MessageKey:    "msg",                            // 日志中消息字段的键名
+		StacktraceKey: "stacktrace",                     // 日志中堆栈跟踪字段的键名
+		LineEnding:    zapcore.DefaultLineEnding,        // 日志行的结束符，默认为 "\n"
+		EncodeLevel:   zapcore.CapitalColorLevelEncoder, // 日志级别的编码方式，这里使用带颜色的编码
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05"))
+		},
+		EncodeDuration: zapcore.StringDurationEncoder, // 日志中持续时间的编码方式，这里使用字符串格式
+		EncodeCaller:   zapcore.ShortCallerEncoder,    // 日志中调用者信息的编码方式，这里使用短格式
+	}
 
 	// 设置日志输出到控制台
 	config.OutputPaths = []string{"stdout"}
 	config.ErrorOutputPaths = []string{"stderr"}
 
 	// 构建 Logger
-	logger, err := config.Build()
+	logger, err := config.Build(zap.AddCaller(), zap.AddCallerSkip(1))
 	if err != nil {
 		panic("Failed to initialize logger: " + err.Error())
 	}
